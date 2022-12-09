fe25ee4dcbb119e66b7e4d02e463d0873559003f
manami-sato <manamisato2001@gmail.com>
2022-12-09 12:31:09 +0900

    トップページ途中

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,32 @@
 import type { NextPage } from 'next';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import OtetsudaiBtn from '../src/components/OtetsudaiBtn';
+import OtetsudaiAdd from '../src/components/OtetsudaiAdd';
 
 const Home: NextPage = () => {
-  return <Box>おてつだいず！</Box>;
+  return (
+    <Box
+      minWidth="100vw"
+      minHeight="100vh"
+      bg="url('img/bg.png')"
+      bgSize="cover"
+      bgPosition="center bottom"
+    >
+      <Box>おてつだいず！</Box>
+      <OtetsudaiBtn />
+      <OtetsudaiAdd />
+      <Flex w="240px" h="56px">
+        <Box
+          as="img"
+          src="img/text_owattayo.png"
+          alt="終わったよ！"
+          w="100%"
+          h="100%"
+          objectFit="contain"
+        />
+      </Flex>
+    </Box>
+  );
 };
 
 export default Home;
---
+++ src/components/OtetsudaiBtn.tsx
@@ -0,0 +1,85 @@
+import { FC, Key } from 'react';
+import { Flex, Text, Box, color } from '@chakra-ui/react';
+import OriginalSpacer from './OriginalSpacer';
+import { categoryType } from '../types/category';
+import { category } from '../libs/category';
+
+import Icon from '../assets/vercel';
+
+const test = [1, 2, 3];
+
+const hoge: {
+  title: string;
+  category: number;
+}[] = [
+  { title: 'くつならべ', category: 2 },
+  { title: 'くつならべ', category: 0 },
+];
+
+const OtetsudaiBtn: FC = () => {
+  return (
+    <Flex as="ul" flexWrap="wrap" gap="24px 16px">
+      {hoge.map(
+        (
+          item: {
+            title: string;
+            category: number;
+          },
+          i: number
+        ) => (
+          <Flex
+            key={item.category + i}
+            alignItems="center"
+            as="li"
+            w="280px"
+            h="80px"
+            color={category[item.category].color.dark}
+            bg="#F5F3E4"
+            fontSize="2.3rem"
+            pl="72px"
+            borderStyle="solid"
+            borderWidth="4px"
+            borderColor={category[item.category].color.light}
+            borderRadius="20px"
+            boxShadow={`4px 4px 0 #FCFBF7 inset, -4px -4px 0 #DDDBCD inset, 0 3px 0 ${
+              category[item.category].color.dark
+            }`}
+            pos="relative"
+            sx={{
+              '&::before': {
+                content: "''",
+                display: 'block',
+                background: category[item.category].color.light,
+                width: '48px',
+                height: '48px',
+                borderRadius: '9999px',
+                position: 'absolute',
+                inset: 'auto auto auto 16px',
+              },
+              '&::after': {
+                content: "''",
+                display: 'block',
+                background: `url("img/icon_${
+                  category[item.category].categoryEn
+                }.svg")`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center',
+                width: '48px',
+                height: '48px',
+                borderRadius: '9999px',
+                position: 'absolute',
+                inset: 'auto auto auto 16px',
+              },
+            }}
+          >
+            {/* <Box as="img" src="img/test.jpg" /> */}
+            {/* <Box as={Icon} sx={{ '>path': { fill: 'skyblue' } }} /> */}
+            {item.title}
+          </Flex>
+        )
+      )}
+    </Flex>
+  );
+};
+
+export default OtetsudaiBtn;
