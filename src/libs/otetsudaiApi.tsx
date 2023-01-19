53a37ec3ac3ddc2dd112f5176317ab566bb2da8e
下河寛宗 <kan.1118.shu@gmail.com>
2023-01-19 23:20:05 +0900

    API変更

--- src/libs/otetsudaiApi.tsx
+++ src/libs/otetsudaiApi.tsx
@@ -1,3 +1,3 @@
 // export const otetsudaiApi = 'https://otetsudai-zoo.vercel.app/api/otetsudai';
-export const otetsudaiApi = 'http://localhost:3000/api/otetsudai';
-// export const otetsudaiApi = 'https://2022team-project.vercel.app/api/otetsudai';
+// export const otetsudaiApi = 'http://localhost:3000/api/otetsudai';
+export const otetsudaiApi = 'https://2022team-project.vercel.app/api/otetsudai';
