54ff645f5756f5efc48ebdd3b1a42fe7abdcb0a3
manami-sato <manamisato2001@gmail.com>
2023-01-19 18:14:42 +0900

    cors突破

--- src/libs/otetsudaiApi.tsx
+++ src/libs/otetsudaiApi.tsx
@@ -1,8 +1,3 @@
-// export const otetsudaiApi = 'https://otetsudai-zoo.vercel.app/api/otetsudai';
-// export const otetsudaiApi = 'https://2022team-project-ap3vof9pq-kanshu-1118.vercel.app/api/otetsudai';
-<<<<<<< HEAD
-export const otetsudaiApi = 'http://localhost:3000/api/otetsudai';
-=======
-export const otetsudaiApi = 'https://2022team-project.vercel.app/api/otetsudai';
+export const otetsudaiApi = 'https://otetsudai-zoo.vercel.app/api/otetsudai';
 // export const otetsudaiApi = 'http://localhost:3000/api/otetsudai';
->>>>>>> cc6a7bbf590377c46696c333071a47b1461af40e
+// export const otetsudaiApi = 'https://2022team-project.vercel.app/api/otetsudai';
