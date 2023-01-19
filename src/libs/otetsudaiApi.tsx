7a1479489260f7e1607f987387683d7c0190e1bb
下河寛宗 <kan.1118.shu@gmail.com>
2023-01-19 17:31:49 +0900

    pullした

--- src/libs/otetsudaiApi.tsx
+++ src/libs/otetsudaiApi.tsx
@@ -1,3 +1,4 @@
 // export const otetsudaiApi = 'https://otetsudai-zoo.vercel.app/api/otetsudai';
-export const otetsudaiApi = 'https://2022team-project-ap3vof9pq-kanshu-1118.vercel.app/api/otetsudai';
-// export const otetsudaiApi = 'http://localhost:3000/api/otetsudai';
+// export const otetsudaiApi = 'https://2022team-project-ap3vof9pq-kanshu-1118.vercel.app/api/otetsudai';
+// export const otetsudaiApi = 'https://2022team-project.vercel.app//api/otetsudai';
+export const otetsudaiApi = 'http://localhost:3000/api/otetsudai';
