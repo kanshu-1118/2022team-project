b8b6c8b948b5f96dc659c41e43e2a1d16e1ebc74
下河寛宗 <kan.1118.shu@gmail.com>
2023-01-19 23:18:20 +0900

    merge完了

--- pages/parents/index.tsx
+++ pages/parents/index.tsx
@@ -17,6 +17,7 @@ import { otetsudaiApi } from '../../src/libs/otetsudaiApi';
 import { otetsudaiType } from '../../src/types/otetsudai';
 import ModalRemoveBtn from '../../src/components/ModalRemoveBtn';
 import EnterBtn from '../../src/components/EnterBtn';
+import NextLink from 'next/link';
 
 const ParentsStart: NextPage = () => {
   const [fireBaseUserData, setFireBaseUserData] = useState<any>('');
@@ -130,57 +131,59 @@ const ParentsStart: NextPage = () => {
   );
   const OtetsudaiAdd = () => {
     return (
-      <Flex alignItems="center" w="fit-content" pos="relative">
-        <Text
-          display="flex"
-          alignItems="center"
-          h="64px"
-          color="pink300"
-          bg="white"
-          p="0 48px 0 20px"
-          fontSize="2rem"
-          borderWidth="4px"
-          borderStyle="solid"
-          borderColor="pink300"
-          borderRadius="9999px"
-        >
-          おてつだいを追加
-        </Text>
-        <Flex
-          justifyContent="center"
-          alignItems="center"
-          w="80px"
-          h="80px"
-          bg="url('../img/parts_highlight.svg'), linear-gradient(#F2798F, #F2798F)"
-          bgRepeat="no-repeat, no-repeat"
-          bgPosition="top 12px left 12px, center"
-          border="4px solid #ffffff"
-          borderRadius="9999px"
-          boxShadow="-3px -3px 0 #ED4076 inset"
-          pos="absolute"
-          inset="auto -40px auto auto"
-          sx={{
-            '&::before': {
-              content: "''",
-              display: 'block',
-              width: '8px',
-              height: '32px',
-              background: 'white',
-              borderRadius: '3px',
-              pos: 'absolute',
-            },
-            '&::after': {
-              content: "''",
-              display: 'block',
-              width: '32px',
-              height: '8px',
-              background: 'white',
-              borderRadius: '3px',
-              pos: 'absolute',
-            },
-          }}
-        />
-      </Flex>
+      <NextLink href={`OtetudaiArray`} passHref>
+        <Flex alignItems="center" w="fit-content" pos="relative">
+          <Text
+            display="flex"
+            alignItems="center"
+            h="64px"
+            color="pink300"
+            bg="white"
+            p="0 48px 0 20px"
+            fontSize="2rem"
+            borderWidth="4px"
+            borderStyle="solid"
+            borderColor="pink300"
+            borderRadius="9999px"
+          >
+            おてつだいを追加
+          </Text>
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            w="80px"
+            h="80px"
+            bg="url('../img/parts_highlight.svg'), linear-gradient(#F2798F, #F2798F)"
+            bgRepeat="no-repeat, no-repeat"
+            bgPosition="top 12px left 12px, center"
+            border="4px solid #ffffff"
+            borderRadius="9999px"
+            boxShadow="-3px -3px 0 #ED4076 inset"
+            pos="absolute"
+            inset="auto -40px auto auto"
+            sx={{
+              '&::before': {
+                content: "''",
+                display: 'block',
+                width: '8px',
+                height: '32px',
+                background: 'white',
+                borderRadius: '3px',
+                pos: 'absolute',
+              },
+              '&::after': {
+                content: "''",
+                display: 'block',
+                width: '32px',
+                height: '8px',
+                background: 'white',
+                borderRadius: '3px',
+                pos: 'absolute',
+              },
+            }}
+          />
+        </Flex>
+      </NextLink>
     );
   };
   const OtetsudaiList = () => {
