0b219962b12566359b47f538cfa522b5692c0d7e
manami-sato <manamisato2001@gmail.com>
2023-01-19 15:13:47 +0900

    parentsがapiのエラーで表示されない問題

--- src/libs/otetsudaiApi.tsx
+++ src/libs/otetsudaiApi.tsx
@@ -1,2 +1,2 @@
-export const otetsudaiApi = 'https://otetsudai-zoo.vercel.app/api/otetsudai';
-// export const otetsudaiApi = 'http://localhost:3000/api/otetsudai';
+// export const otetsudaiApi = 'https://otetsudai-zoo.vercel.app/api/otetsudai';
+export const otetsudaiApi = 'http://localhost:3000/api/otetsudai';
